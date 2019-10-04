33605f010be4d61896ba60d558621bcd2757536d
Chris Oakman <chris@oakmac.com>
2019-10-03 20:30:36 -0500

    added the worlds simplest form

--- index.js
+++ index.js
@@ -14,10 +14,22 @@ const port = 3000
 
 const homepageTemplate = fs.readFileSync('./templates/homepage.mustache', 'utf8')
 
+app.use(express.urlencoded())
+
 app.get('/', function (req, res) {
   getAllCohorts()
     .then(function (allCohorts) {
-      res.send(mustache.render(homepageTemplate, {cohortsListHTML: renderAllCohorts(allCohorts)}))
+      res.send(mustache.render(homepageTemplate, { cohortsListHTML: renderAllCohorts(allCohorts) }))
+    })
+})
+
+app.post('/cohorts', function (req, res) {
+  createCohort(req.body)
+    .then(function () {
+      res.send('hopefully we created your cohort <a href="/">go home</a>')
+    })
+    .catch(function () {
+      res.status(500).send('something went wrong. waaah, waaah')
     })
 })
 
@@ -60,13 +72,17 @@ function getAllCohorts () {
 
 function getOneCohort (slug) {
   return db.raw('SELECT * FROM Cohorts WHERE slug = ?', [slug])
-         .then(function (results) {
-           if (results.length !== 1) {
-             throw null
-           } else {
-             return results[0]
-           }
-         })
+    .then(function (results) {
+      if (results.length !== 1) {
+        throw null
+      } else {
+        return results[0]
+      }
+    })
+}
+
+function createCohort (cohort) {
+  return db.raw('INSERT INTO Cohorts (title, slug, isActive) VALUES (?, ?, true)', [cohort.title, cohort.slug])
 }
 
 // -----------------------------------------------------------------------------
